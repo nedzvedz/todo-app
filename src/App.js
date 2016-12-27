7c351af6b7a34de7c3f1225222c5368bdb454e45
Vitali Nedzvedz <vitali_nedzvedz@epam.com>
2016-12-27 20:36:32 +0300

    Relace react-scripts dependency

--- src/App.js
+++ src/App.js
@@ -15,9 +15,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <div className="App-content">
           <Card className="App-card">
             <AddTodoContainer />
