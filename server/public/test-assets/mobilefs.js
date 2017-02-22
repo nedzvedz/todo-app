a5627bee45ae0cc5cbf5948ffd706020430a8e57
Vitali Nedzvedz <vitali_nedzvedz@epbyminw1775.minsk.epam.com>
2017-02-22 11:50:11 +0300

    Added test files

---
+++ server/public/test-assets/mobilefs.js
@@ -0,0 +1,1 @@
+function setCookie(){var o=new Date;o.setTime(o.getTime()+12e5),document.cookie="nomobile=1; domain=."+location.hostname+";host=."+location.hostname+"; expires="+o.toGMTString()+";path=/"}
